9f84cc61172bb5efc6a9c2034387690527eb13db
YmClash <momo.toure27@gmail.com>
2023-04-26 01:14:52 +0200

    some minor update for the bot and bug fix Spotify Feature added

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+
+
+26/04/2023
+
+adding Spotify  feature
+
+
+24/04/2023
+adding  GPT3.5-turbo model and  Dall-e model to bot 
---
+++ spotify.py
@@ -0,0 +1,30 @@
+import os
+
+import spotipy
+from dotenv import load_dotenv
+from spotipy import SpotifyClientCredentials
+load_dotenv()
+
+SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+
+auth_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET)
+
+spoti = spotipy.Spotify(client_credentials_manager=auth_manager)
+
+
+uri = '6l3HvQ5sa6mXTsMTB19rO5'
+
+
+result = spoti.artist_albums(uri, album_type='album')
+albums = result['items']
+
+
+while result['next']:
+    result = spoti.next(result)
+    albums.extend(result['items'])
+
+for album in albums:
+    print(album['name'])
+
+
